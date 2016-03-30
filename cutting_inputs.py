af7295b1a2226a35ab4d769900f50917d842b876
nikhil <Nikhil12321>
2016-03-31 02:18:38 +0530

    same_old

--- cutting_inputs.py
+++ cutting_inputs.py
@@ -4,13 +4,27 @@
 import csv
 
 
-all_inputs = csv.reader(open("output"+".csv", 'r'), delimiter=',')
+all_inputs = csv.reader(open("oo"+".csv", 'r'), delimiter=',')
+
+max_full_stops = 0
+
+
+for row in all_inputs:
+	review = row[1]
+	full_stops = 0
+	for char in review:
+		if(char == "."):
+			full_stops += 1
+		if(full_stops > max_full_stops):
+			max_full_stops = full_stops
+
+
 file_number = 0
 path = "breaks/"
 file_name_tag = "break_at_"
 global_doc = {}
 
-for x in xrange(1, 3):
+for x in range(1, max_full_stops):
 	global_doc[x] = []
 
 for row in all_inputs:
@@ -24,11 +38,16 @@ for row in all_inputs:
 			l.append(sentiment)
 			l.append(str(sentence))
 			file_number += 1
-			if(file_number > 2):
-				continue
 			global_doc[file_number].append(l)
 		else:
 			sentence += char
+
+	# if (file_number < max_full_stops):
+	# 	for x in range(file_number+1, max_full_stops):
+	# 		l = []
+	# 		l.append(sentiment)
+	# 		l.append("")
+	# 		global_doc[x].append(l)
 
 for key in global_doc:
  	with open(path + file_name_tag + str(key) + ".csv", "wb") as csv_writer:
