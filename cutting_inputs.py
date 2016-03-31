42354af84f077dfbf8f441fa2500d5cbbd4524f0
nikhil <Nikhil12321>
2016-03-31 23:52:21 +0530

    clear

--- cutting_inputs.py
+++ cutting_inputs.py
@@ -4,27 +4,32 @@
 import csv
 
 
-all_inputs = csv.reader(open("oo"+".csv", 'r'), delimiter=',')
+def get_max_full_stops():
 
-max_full_stops = 0
-
-
-for row in all_inputs:
-	review = row[1]
-	full_stops = 0
-	for char in review:
-		if(char == "."):
-			full_stops += 1
-		if(full_stops > max_full_stops):
-			max_full_stops = full_stops
+	all_inputs = csv.reader(open("output"+".csv", 'r'), delimiter=',')
+	max_full_stops = 0
+	for row in all_inputs:
+		review = row[1]
+		full_stops = 0
+		for char in review:
+			if(char == "."):
+				full_stops += 1
+			if(full_stops > max_full_stops):
+				max_full_stops = full_stops
+	return max_full_stops
 
 
 file_number = 0
 path = "breaks/"
 file_name_tag = "break_at_"
 global_doc = {}
+all_inputs = csv.reader(open("oo"+".csv", 'r'), delimiter=',')
 
-for x in range(1, max_full_stops):
+max_full_stops = get_max_full_stops()
+print "max full stops "
+print max_full_stops
+
+for x in range(1, max_full_stops+1):
 	global_doc[x] = []
 
 for row in all_inputs:
@@ -38,16 +43,17 @@ for row in all_inputs:
 			l.append(sentiment)
 			l.append(str(sentence))
 			file_number += 1
+			print file_number
 			global_doc[file_number].append(l)
 		else:
 			sentence += char
 
-	# if (file_number < max_full_stops):
-	# 	for x in range(file_number+1, max_full_stops):
-	# 		l = []
-	# 		l.append(sentiment)
-	# 		l.append("")
-	# 		global_doc[x].append(l)
+	if (file_number < max_full_stops):
+	 	for x in range(file_number+1, max_full_stops):
+	 		l = []
+	 		l.append(sentiment)
+	 		l.append("")
+	 		global_doc[x].append(l)
 
 for key in global_doc:
  	with open(path + file_name_tag + str(key) + ".csv", "wb") as csv_writer:
